aa429e8a8e7b028e6227702648e24dac76a83a84
Dan Siegel <me@dansiegel.net>
2023-08-21 14:24:40 -0600

    chore: update control loading with loading messages

--- ArcGisEmbeddingSample.MauiControls/EsriSamplePage.xaml.cs
+++ ArcGisEmbeddingSample.MauiControls/EsriSamplePage.xaml.cs
@@ -1,6 +1,8 @@
+using System.Runtime.ConstrainedExecution;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Mapping.Floor;
+using Esri.ArcGISRuntime.Maui;
 using Esri.ArcGISRuntime.Rasters;
 using Map = Esri.ArcGISRuntime.Mapping.Map;
 
@@ -10,6 +12,7 @@ public partial class EsriSamplePage : ContentView
 {
 	private FloorManager _floorManager;
 	private readonly Dictionary<string, FloorLevel> _floorOptions = new Dictionary<string, FloorLevel>();
+	private readonly MapView MyMapView = new MapView();
 
 	public EsriSamplePage()
 	{
@@ -27,7 +30,9 @@ public partial class EsriSamplePage : ContentView
 			MyMapView.Map = map;
 
 			// Map needs to be loaded in order for floormanager to be used.
+			LoadingLabel.Text = "Loading Map....";
 			await MyMapView.Map.LoadAsync();
+			LoadingLabel.Text = "Checking Floor Manager....";
 			List<string> floorName = new List<string>();
 
 			// Checks to see if the layer is floor aware.
@@ -43,20 +48,33 @@ public partial class EsriSamplePage : ContentView
 				return;
 			}
 
+			LoadingLabel.Text = "Loading Floor Manager...";
 			await MyMapView.Map.FloorManager.LoadAsync();
 			_floorManager = MyMapView.Map.FloorManager;
 
 			// Use the dictionary to add the level's name as the key and the FloorLevel object with the associated level's name.
+			LoadingLabel.Text = "Getting Floor Levels...";
 			foreach (FloorLevel level in _floorManager.Facilities[0].Levels)
 			{
 				_floorOptions.Add(level.ShortName, level);
 				floorName.Add(level.ShortName);
 			}
 
+			Content = MyMapView;
 			//FloorChooser.ItemsSource = floorName;
 		}
 		catch (Exception ex)
 		{
+			Content = new ScrollView
+			{
+				Content = new VerticalStackLayout()
+				{
+					new Label { Text = "An Unexpected Error Occurred:", FontAttributes = FontAttributes.Bold },
+					new Label { Text = ex.GetType().FullName },
+					new Label { Text = ex.Message },
+					new Label { Text = ex.StackTrace?.ToString() },
+				}
+			};
 			System.Diagnostics.Debug.WriteLine(ex.Message);
 		}
 	}
